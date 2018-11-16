ca46fb71a1bd871944946018d4eaaa4319cc83e6
Kele <optimirk@gmail.com>
2018-11-16 17:32:55 +0100

    Første forsøg med databasen

---
+++ service/database.js
@@ -0,0 +1,64 @@
+function DB() {
+  // Get a reference to the database service
+  this.database = firebase.database();
+  this.beskeder = [];
+
+  this.addmes = function (mes) {
+    var besked = {
+      fra: "ukendt",
+      besked: mes,
+      oprettet: Date.now()
+    };
+
+    var k = firebase.database().ref().child('posts').push().key;
+    firebase.database().ref('/beskeder/' + k).set(besked);
+  }
+
+  this.listen = function () {
+    // var userId = firebase.auth().currentUser.uid;
+    var db = this;
+    return firebase.database().ref("beskeder/").on('value', function(snapshot) {
+      var beskeder = snapshot.val();
+      for (var k in beskeder) {
+        db.beskeder.push(beskeder[k]);
+      }
+
+      app.setBeskeder(db.beskeder);
+      console.log("Du har skrevet: ", snapshot.val());
+    });
+  }
+
+  this.listen();
+}
+
+function User() {
+
+  this.signin = function (email, password) {
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ...
+      console.error("Fejl med at logge ind", error);
+    });
+  }
+
+  firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+    var displayName = user.displayName;
+    var email = user.email;
+    var emailVerified = user.emailVerified;
+    var photoURL = user.photoURL;
+    var isAnonymous = user.isAnonymous;
+    var uid = user.uid;
+    var providerData = user.providerData;
+
+    console.log("Du er logget ind som", user);
+  } else {
+    // User is signed out.
+    // ...
+    console.log("Du er logget af");
+  }
+});
+}
